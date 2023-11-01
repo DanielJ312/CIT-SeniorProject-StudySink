557b6d8ee5d79036ae901104218f5b951a592318
DanielJ312 <danieljavaherian13@gmail.com>
2023-10-31 22:40:46 -0700

    Added comments explaining page & cleaned up

--- forum/forum.js
+++ forum/forum.js
@@ -1,3 +1,4 @@
+// Forum.js - Runs any javascript function for the forum
 function DeleteComment(commentIDToDelete) {
     $.ajax({
         url:"post-template.php",    //the page containing php script
@@ -12,64 +13,25 @@ function TestFunction(test) {
     console.log(test);
 }
 
-
-// async function sortType() {
-// $(document).ready(function() {
-/*
-$(".sort").on("change", function(){
-    var type = $(this).val();
-    console.log(type);
+// Function to update the sorted data
+function updateSortedData(sortType) {
     $.ajax({
-        url: '/forum/index.php',
+        url: 'sort.php', 
         type: 'POST',
-        data: {sortType: type},
+        data: { sortType: sortType },
         success: function(response) {
-            // Handle the response, which may contain the sorted data
-            // Update your page with the sorted data
-            $("#result").html(response);
+            $('.forum-posts').html(response); // Update the container with sorted data
         },
+        error: function(xhr, status, error) {
+            console.error(error);
+        }
     });
+}
+
+// Event handler for select change
+$('.sort').on('change', function() {
+    var sortType = $(this).val(); // Get the selected value
+    updateSortedData(sortType); // Call the function to update the sorted data
+    console.log(sortType);
 });
-*/
-
-// });
-// }
-// sortType();
-
-
-// $("select option").filter(function() {
-//     //may want to use $.trim in here
-//     return $(this).text() == text;
-//   }).val('selected');
-
-
-// const selectElement = document.querySelector(".sort");
-// selectElement.addEventListener('change', (event) => {
-//     // Get the selected option's value
-//     const selectedValue = selectElement.value;
-//     console.log(selectedValue);
-// });
-
-// $(document).ready(function() {
-    // Function to update the sorted data
-    function updateSortedData(sortType) {
-        $.ajax({
-            url: 'sort.php', 
-            type: 'POST',
-            data: { sortType: sortType },
-            success: function(response) {
-                $('.forum-posts').html(response); // Update the container with sorted data
-            },
-            error: function(xhr, status, error) {
-                console.error(error);
-            }
-        });
-    }
-
-    // Event handler for select change
-    $('.sort').on('change', function() {
-        var sortType = $(this).val(); // Get the selected value
-        updateSortedData(sortType); // Call the function to update the sorted data
-        console.log(sortType);
-    });
 // });
