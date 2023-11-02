c93a7a3e0cf28f6a8e3510e0c0d2d70e36de1b1b
DanielJ312 <danieljavaherian13@gmail.com>
2023-11-01 20:56:51 -0700

    Implemented Bryce's new navigation bar

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,16 @@
 <!-- Head - Contains HTML injected into the head tag -->
+<!-- Main -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= isset($pageTitle) ? $pageTitle : "" ?></title>
 <link rel="stylesheet" href="/styles.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<!-- <link rel="icon" type="image/x-icon" href="/images/favicon.ico"> -->
+<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+
+<!-- Navbar -->
+<link rel="stylesheet" href="/styles/header.css">
+<script defer src="/includes/header.js"></script>
+<script src="https://kit.fontawesome.com/f0dbd56a8f.js" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,74 @@
 <!-- Header - Contains HTML injected into the header tag -->
-<h1>StudySink Backend Development</h1>
+<!-- Primary Navbar -->
+<div class="body">
+    <div class="navbarMain">
+        <div class="navbar-left">
+            <a href="Home" id="Home"><img src="StudySinkWords.png" alt="Company Logo" class="companyLogo" title="Home"></a>
+        </div>
+        <div class="navbar-center">
+            <div class="search-container">
+                <a href="SearchResults"><i class="fa-solid fa-magnifying-glass fa-xl" style="color: #000000;"></i></a>
+                <input type="text" id="searchBar" placeholder="Search Study sets, Universities, Posts" style="padding-left: 35px;">
+            </div>
+        </div>
+        <div class="navbar-right">
+            <div class="dropdown" style="padding-top: 15px; padding-bottom: 15px;">
+                <i class="fa-solid fa-circle-plus fa-2xl" id="createIcon" title="Create" style="cursor: pointer;"></i>
+                    <div class="dropdown-content-create" id="createDropdown">
+                    <a href="CreateStudySet">Create Study Set</a>
+                    <a href="CreatePost">Create Post</a>
+                    </div>
+            </div>
+            <a href="Home" id="Home" title="Home"><i class="fa-solid fa-house fa-2xl"></i></a>
+            <a href="UniversityPage" id="University" title="My University"><i class="fa-solid fa-graduation-cap fa-flip-horizontal fa-2xl"></i></a>
+            <div class="dropdown">
+                <img src="pp.png" alt="Place Holder" class="profile-picture" id="profilePicture" title="Profile">
+                <div class="dropdown-content-profile" id="profileDropdown">
+                    <a href="Profile">Profile</a>
+                    <a href="Settings">Settings</a>
+                    <a href="Help/Request">Help/Request University</a>
+                    <a href="Logout">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbarmobile">
+        <header style="height: 20px;">
+            <i class="fa-solid fa-bars fa-2xl" id="menuIcon" title="Menu Icon"></i>
+            <img id="logo" src="StudySinkwords.png" alt="Company Logo" title="Home" style="margin-top: -10px; margin-bottom: -10px;">
+            <nav>
+                <div>
+                    <form id="search-form" class="searchform">
+                        <a href="SearchResults" class="mag"><i class="fa-solid fa-magnifying-glass fa-sm" style="color: #000000; padding-left: 2px; margin-bottom: 3px"></i></a>
+                        <input type="text" id="search-input" placeholder="Search" class="searchbar" title="search">
+                    </form>
+                </div>
+                <div class="nav-options">
+                    <a href="Home" title="Home">Home</a>
+                    <a href="University" title="My University">My University</a>
+                    <div class="dropdown">
+                        <a href="#" title="Create">Create</a>
+                        <div class="dropdown-content">
+                            <a href="StudySetCreate" title="Create Study Set">Study Set</a>
+                            <a href="postCreate" title="Create Post">Post</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <a href="#" style="border-bottom-color: black; border-bottom-width: 2px; border-bottom-style: solid;" title="Profile">Profile</a>
+                        <div class="dropdown-content">
+                            <a href="Profile" style="border-top-width: 0px;" title="My Profile">My Profile</a>
+                            <a href="Settings" title="Settings">Settings</a>
+                            <a href="Help" title="Help">Help/Request Uni</a>
+                            <a href="Logout" style="border-bottom-color: black; border-bottom-width: 2px; border-bottom-style: solid;" title="Logout">Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        <header>
+    </div>
+</div>
+
+<!-- Development Navbar For Easy Access -->
 <nav class="navbar">
     <span>Development Navbar:</span>
     <a class="<?php check_active_page('/index.php');?>" href="/index.php">Home</a>
@@ -12,17 +81,18 @@
     <a class="<?php check_active_page('/account/logout.php');?>" href="/account/logout.php">Logout</a>
     <a class="<?php check_active_page('/account/forgot.php');?>" href="/account/forgot.php">Reset</a>
     <div style="float:right;">
-        <?php if (!isset($_SESSION['USER'])): ?>
-            <a class="dropdown" href="/account/login.php">Login</a>
-        <?php else: ?>
-            <div class="dropdown">
-                <button class="dropbtn <?php check_active_page('/account/profile.php');?>"><?=$_SESSION['USER']->Username?></button>
-                <div class="dropdown-content">
-                    <a class="<?php check_active_page('/account/profile.php');?>" href="/account/profile.php">Profile</a>
-                    <a href="">Settings</a>
-                    <a href="/account/logout.php">Logout</a>
-                </div>
+    <?php if (!isset($_SESSION['USER'])): ?>
+        <a class="dropdown" href="/account/login.php">Login</a>
+    <?php else: ?>
+        <div class="dropdown">
+            <button class="dropbtn <?php check_active_page('/account/profile.php');?>"><?=$_SESSION['USER']->Username?></button>
+            <div class="dropdown-content">
+                <a class="<?php check_active_page('/account/profile.php');?>" href="/account/profile.php">Profile</a>
+                <a href="">Settings</a>
+                <a href="/account/logout.php">Logout</a>
             </div>
-        <?php endif; ?>
+        </div>
+    <?php endif; ?>
     </div>
 </nav>
+<h1>StudySink Backend Development</h1>
