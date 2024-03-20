0ae6d428d4f30c4c43497f6bbb2e26e386b48a32
Lafawnduh <Bryceghouck@gmail.com>
2024-03-19 18:44:13 -0700

    Footer
    Started the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,13 @@
 <!-- Footer - Contains HTML injected into the footer tag -->
+<head>
+    <link rel="stylesheet" type="text/css" href="/styles/includes/footer.css">
+</head>
+<ul>
+    <li><a href="about-us.html">About us</a></li>
+    <li><a href="https://github.com/your-repo">Source Code</a></li>
+    <li><a href="contact-support.html">Contact Support</a></li>
+    <li><a href="https://docs.google.com/document/d/your-document-id">Documentation</a></li>
+    <li><a href="copyright-notice.html">Copyright notice</a></li>
+    <li><a href="privacy-policy.html">Privacy Policy</a></li>
+</ul>
+<img src="logo.png" alt="Logo">
