1655db16a9841bb322b5b903bfc346ac472f9089
DanielJ312 <danieljavaherian13@gmail.com>
2024-04-22 10:28:49 -0700

    Fixed about.php css

--- info/about.php
+++ info/about.php
@@ -8,7 +8,7 @@ $pageTitle = "About";
 <html lang="en">
 <head>
     <?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/head.php"); ?>
-    <link rel="stylesheet" type="text/css" href="/styles/info/privacy-policy.css">
+    <link rel="stylesheet" type="text/css" href="/styles/info/privacy.css">
 </head>
 <body>
     <header>
